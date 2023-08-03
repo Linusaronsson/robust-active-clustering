fd8d4e4d0bdd9c96c9f25d4af68412bde25a1cae
Linus Aronsson <LinusAronsson.96@hotmail.com>
2023-08-03 18:22:53 +0200

    .

--- copy_results.sh
+++ copy_results.sh
@@ -8,4 +8,7 @@ scp -r linaro@alvis1.c3se.chalmers.se:/mimer/NOBACKUP/groups/snic2022-22-883 C:\
 
 scp -r linaro@alvis1.c3se.chalmers.se:/cephyr/users/linaro/Alvis/correlation-clustering/experiment_results_local/ C:\Github_projects/correlation-clustering/experiment_results_local/
 
-scp -r linaro@alvis1.c3se.chalmers.se:/cephyr/users/linaro/Alvis/robust-active-clustering/experiment_results_local/ C:\Github_projects/robust-active-clustering/experiment_results_local/
+
+
+
+scp -r linaro@alvis1.c3se.chalmers.se:/cephyr/users/linaro/Alvis/robust-active-clustering/rac/experiment_results_local/ C:\Github_projects/robust-active-clustering/experiment_results_local/
