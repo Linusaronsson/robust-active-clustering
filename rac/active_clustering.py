f2c4a17b739d1ac9cad41f4dff0ee47b6ae2a28a
Linus Aronsson <LinusAronsson.96@hotmail.com>
2023-10-04 15:02:42 +0200

    .

--- rac/active_clustering.py
+++ rac/active_clustering.py
@@ -663,6 +663,9 @@ class ActiveClustering:
         indices1 = self.random.choice(cond1, np.min([len(cond1), len(cond2), 5000]))
         indices2 = self.random.choice(cond2, len(indices1))
 
+        indices1 = cond1
+        indices2 = cond2
+
         indices = np.concatenate([indices1, indices2])
         ind1, ind2 = lower_triangle_indices[0][indices], lower_triangle_indices[1][indices]
         print("HERE: ", self.X.shape)
